3690be62e63d3cec6dcd19f2e0f5be6d89a9b3f3
ayyubnurumi <ayyubinvestment@gmail.com>
2023-05-18 23:49:15 +0700

    update services

--- src/pages/Dasboard.jsx
+++ src/pages/Dasboard.jsx
@@ -1,12 +1,17 @@
 // import React from 'react'
+import { useEffect } from "react";
 import emptyActivity from "../assets/activity-empty-state.png";
 import { ActivityCard } from "../components/ActivityCard";
 import { activity } from "../services/services";
 
 export const Dasboard = () => {
+  const [get, create,,] = activity
   const addActivity = () => {
-    activity.getActivity()
+    create()
   }
+  useEffect(() => {
+    get()
+  }, [get]);
   return (
     <>
       <div className="section-header">
@@ -14,9 +19,6 @@ export const Dasboard = () => {
         <button onClick={addActivity} className="add-btn">+ tambah</button>
       </div>
       <div className="section-body">
-        <ActivityCard />
-        <ActivityCard />
-        <ActivityCard />
         <ActivityCard />
         <img style={{flex: "0 1 100%", marginBottom: "30px"}} src={emptyActivity} alt="empty-state" />
       </div>
--- src/services/services.js
+++ src/services/services.js
@@ -2,10 +2,13 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://todo.api.devcode.gethired.id/";
 
-const getActivity = () => {
+const getActivity = (setData) => {
   axios
-    .get("activity-groups")
+    .get("activity-groups", {
+      params: { email: "ayyubbabdulrahman17@gmail.com" },
+    })
     .then((res) => {
+      setData(res.data)
       console.log(res);
     })
     .catch((err) => {
@@ -20,7 +23,9 @@ const createActivity = () => {
       email: "ayyubabdulrahman17@gmail.com",
     })
     .then((res) => {
-      console.log(res);
+      const data = res.data;
+      getActivity()
+      console.log(data);
     })
     .catch((err) => {
       console.error(err);
@@ -49,9 +54,55 @@ const removeActivity = ({ id }) => {
     });
 };
 
-export const activity = {
+export const activity = [
   getActivity,
   createActivity,
   updateActivity,
   removeActivity,
+];
+
+const getItem = ({ actid }) => {
+  axios
+    .get("todo-items", { params: { activity_group_id: { actid } } })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 };
+
+const createItem = () => {
+  axios
+    .post("todo-items")
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+const updateItem = ({ id }) => {
+  axios
+    .patch(`todo-items/${id}`)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+const removeItem = ({ id }) => {
+  axios
+    .delete(`todo-items/${id}`)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+export const ToDoItem = [getItem, createItem, updateItem, removeItem];
